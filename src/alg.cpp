da634eb9daa698a0c39240b08af2ddb276b82079
Дмитрий Чипижко <78786848+Chizad4o@users.noreply.github.com>
2021-04-05 13:04:21 +0300

    Чипижко Дмитрий

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,40 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
+int cbinsearch(int *arr, int size, int value) { 
   //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+	int l = 0;
+	int r = size - 1;
+	int mid;
+	int count = 0;
+	bool flag = false;
+	while ((1 <= r) && (flag != true))
+	{
+		mid = (l + r) / 2;
+		if (arr[mid] == value)
+		{
+			flag = true;
+			count += 1;
+			int i = 1;
+			while (arr[mid + i] == value)
+			{
+				count += 1;
+				i += 1;
+			}
+			i = 1;
+			while (arr[mid - i] == value)
+			{
+				count += 1;
+				i += 1;
+			}
+		}
+		else if (arr[mid] > value)
+		{
+			r = mid - 1;
+		}
+		else
+		{
+			l = mid + 1;
+		}
+	}
+  return count;
 }
