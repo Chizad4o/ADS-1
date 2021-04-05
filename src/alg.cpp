37e06a3be1052e8638bdd4a829b1e0dde3a5710f
Дмитрий Чипижко <78786848+Chizad4o@users.noreply.github.com>
2021-04-05 13:22:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,40 +1,35 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) { 
-  //  поместить сюда реализацию алгоритма
-	int l = 0;
-	int r = size - 1;
-	int mid;
-	int count = 0;
-	bool flag = false;
-	while ((1 <= r) && (flag != true))
-	{
-		mid = (l + r) / 2;
-		if (arr[mid] == value)
-		{
-			flag = true;
-			count += 1;
-			int i = 1;
-			while (arr[mid + i] == value)
-			{
-				count += 1;
-				i += 1;
-			}
-			i = 1;
-			while (arr[mid - i] == value)
-			{
-				count += 1;
-				i += 1;
-			}
-		}
-		else if (arr[mid] > value)
-		{
-			r = mid - 1;
-		}
-		else
-		{
-			l = mid + 1;
-		}
-	}
-  return count;
+int cbinsearch(int *arr, int size, int value) {
+	//  поместить сюда реализацию алгоритма
+    int l = 0;
+    int r = size - 1;
+    int mid;
+    int count = 0;
+    bool flag = false;
+    while ((1 <= r) && (flag != true)) {
+        mid = (l + r) / 2;
+        if (arr[mid] == value) {
+            flag = true;
+            count += 1;
+            int i = 1;
+            while (arr[mid + i] == value) {
+                count += 1;
+                i += 1;
+            }
+            i = 1;
+            while (arr[mid - i] == value) {
+                count += 1;
+                i += 1;
+            }
+        }
+        else if (arr[mid] > value) {
+            r = mid - 1;
+        }
+        else
+        {
+            l = mid + 1;
+        }
+    }
+    return count;
 }
