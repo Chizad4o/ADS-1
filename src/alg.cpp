48a6f4a5cde575f2e53dfdd7256e55606d75c706
Дмитрий Чипижко <78786848+Chizad4o@users.noreply.github.com>
2021-04-06 10:59:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,19 +3,19 @@
 int cbinsearch(int *arr, int size, int value) {
     //  поместить сюда реализацию алгоритма
     int l = 0;
-    int r = size - 1;
+    int r = size;
     int mid;
     int count = 0;
     while (l < r) {
         mid = (l + r) / 2;
         if (arr[mid] == value) {
             for (int j = mid; arr[j] == value; j++) {
-                if (arr[mid + j] == value) {
+                if (arr[j] == value) {
                     count++;
                 }
             }
             for (int i = mid-1; arr[i] == value; i--) {
-                if (arr[mid - i] == value) {
+                if (arr[i] == value) {
                     count++;
                 }
             }
